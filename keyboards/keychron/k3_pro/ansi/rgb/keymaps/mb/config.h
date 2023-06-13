da248e091a27ec35dc4081cc4f9a8a4f15a39ee5
Martin Balfanz <me@martinbalfanz.com>
2023-06-13 17:14:47 +0200

    Add some comments

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/mb/config.h
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/mb/config.h
@@ -9,7 +9,11 @@
 #define RGB_MATRIX_DEFAULT_HUE 110
 
 #define TAPPING_TERM 215
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 #define CAPS_WORD_IDLE_TIMEOUT 5000
